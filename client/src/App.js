c8254dc3f6fad285de78d4229ee0d0cc7adabc06
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-01-28 00:03:56 +0000

    client - react router dom intalled and partials file added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+
+    </Router>
   );
 }
 
